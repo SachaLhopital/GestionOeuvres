fc3b5a12ea9362f3c7fa4ab50da56ac02e82a581
Lafaye Yoan <yoan.lafaye-de-micheaux@etu.univ-lyon1.fr>
2017-02-15 15:33:09 +0100

    service proprietaire

--- src/dao/ServiceProprietaire.java
+++ src/dao/ServiceProprietaire.java
@@ -55,4 +55,71 @@ public class ServiceProprietaire {
 
         return mesProprietaires;
     }
+
+    public Proprietaire insererProprietaire(Proprietaire prop){
+        String rq = "insert into proprietaire (nom_proprietaire, prenom_proprietaire) values "+
+                "('"+prop.getNomProprietaire()+"',"+
+                "'"+ prop.getPrenomProprietaire()+"')";
+
+        DialogueBd unDialogueBd = DialogueBd.getInstance();
+        try{
+            unDialogueBd.insertionBD(rq);
+        }catch(Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+
+        //recuperation de l'ID
+        rq = "select LAST_INSERT_ID();";
+
+        List<Object> rs = new ArrayList<>();
+        try
+        {
+            rs = DialogueBd.lecture(rq);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+
+        if(rs.size()<=0){
+            return null;
+        }
+
+        prop.setIdProprietaire(Integer.parseInt(rs.get(0).toString()));
+
+        return prop;
+    }
+
+    public Proprietaire modifierProprietaire(Proprietaire prop){
+        String rq = "update proprietaire set"+
+                "nom_proprietaire='"+prop.getNomProprietaire()+"',"+
+                "prenom_proprietaire='"+prop.getNomProprietaire()+"'"+
+                " where id_proprietaire="+prop.getIdProprietaire();
+
+        DialogueBd unDialogueBd = DialogueBd.getInstance();
+        try{
+            unDialogueBd.execute(rq);
+        }catch(Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+
+        return prop;
+    }
+
+    public void supprimerProprietaire (Proprietaire prop){
+        String rq = "delete from proprietaire where id_proprietaire="+prop.getIdProprietaire();
+
+        DialogueBd unDialogueBd = DialogueBd.getInstance();
+        try{
+            unDialogueBd.execute(rq);
+        }catch(Exception ex){
+            ex.printStackTrace();
+            return;
+        }
+
+        //suppression des oeuvres associées
+        new ServiceOeuvre().supprimerOeuvrepret(prop);
+        new ServiceOeuvre().supprimerOeuvrevente(prop);
+    }
 }
